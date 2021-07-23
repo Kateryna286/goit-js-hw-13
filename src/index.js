f14cecaab1d401b2a8e513b7ac150085057b9bd8
Kateryna Karpova <katyakarpova@gmail.com>
2021-07-23 19:45:39 +0200

    add scc styles

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import axios from 'axios';
 import { Notify } from 'notiflix';
 import imageCardTpl from './templates/imgcard.hbs';
 
-
 const refs = {
     imageGallery: document.querySelector('.gallery'),
     searchForm: document.querySelector('#search-form'),
@@ -16,74 +15,66 @@ const refs = {
 
 var lightbox = new SimpleLightbox('.gallery a');
 axios.defaults.baseURL = 'https://pixabay.com/api';
-const OPTIONS = '&image_type=photo&orientation=horizontal&safesearch=true&per_page=20';
+
+const OPTIONS = '&image_type=photo&orientation=horizontal&safesearch=true&per_page=40';
 const APIKEY = '22564694-3177f5daba1f2572eee652a36';
 let keyWord = '';
-let pageNum = 1;
+let pageNum;
 
 refs.btnLoadMore.classList.add('is-hidden');
+
 refs.searchForm.addEventListener('submit', onSearch);
 refs.btnLoadMore.addEventListener('click', onLoadMore);
 
 function onSearch(event) {
     event.preventDefault();
+    refs.btnLoadMore.classList.add('is-hidden');
     pageNum = 1;
+
     const form = event.currentTarget;
     keyWord = form.elements.searchQuery.value.trim();
+
     clearMarkup();
+
     if (keyWord !== "") {
         fetchImagesByKeyWord(keyWord)
-            .then(images => {
-                console.log(images.hits.length);
-                if (images.hits.length === 0) {
-                    console.log(images.totalHits.length);
-                    Notify.failure(
-                        'Sorry, there are no images matching your search query. Please try again.',
-                        {
-                            timeout: 2000,
-                        });
-                }
+        .then(images => {
+                
+            if (images.hits.length === 0) {               
+                Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+            }
 
-                else {
-                    renderImagesCardsMarkup(images);
-                        const totalHits = images.totalHits;
-                        Notify.info(
-                            `Hooray! We found ${totalHits} images.`,
-                            {
-                                timeout: 2000,
-                            });
-                    console.log(totalHits);
-                    lightbox.refresh();
-                    refs.btnLoadMore.classList.remove('is-hidden');
-                };
+            else {
+                renderImagesCardsMarkup(images);
 
-        
+                const totalHits = images.totalHits;
+                Notify.info(`Hooray! We found ${totalHits} images.`,);
 
-            })
-            .catch(error => console.log(error));
+                lightbox.refresh();
+                refs.btnLoadMore.classList.remove('is-hidden');
+            };
+        })
+        .catch(error => console.log(error));
     };
 };
 
 function onLoadMore(event) {
     event.preventDefault();
     incrementPageNum();
+
     fetchImagesByKeyWord(keyWord)
-        .then(images => {
-            if (images.hits.length === 0) {
-                    console.log(images.totalHits.length);
-                    Notify.failure(
-                        "We're sorry, but you've reached the end of search results.",
-                        {
-                            timeout: 2000,
-                        });
-                refs.btnLoadMore.classList.add('is-hidden');
-            }
-            else {
-                renderImagesCardsMarkup(images);
-                lightbox.refresh();
-            }
-        })
-        .catch(error => console.log(error));
+    .then(images => {
+        if (images.hits.length === 0) {       
+            Notify.failure("We're sorry, but you've reached the end of search results.");
+            refs.btnLoadMore.classList.add('is-hidden');
+        }
+
+        else {
+            renderImagesCardsMarkup(images);
+            lightbox.refresh();
+        }
+    })
+    .catch(error => console.log(error));
 };
 
 async function fetchImagesByKeyWord(keyWord) {
@@ -97,7 +88,7 @@ function createImagesCardsMarkup(images) {
 
 function renderImagesCardsMarkup(images) {
     const markup = createImagesCardsMarkup(images);   
-    refs.imageGallery.insertAdjacentHTML('beforeend', markup);
+    const markupCreating = refs.imageGallery.insertAdjacentHTML('beforeend', markup);
 };
 
 function clearMarkup() {
@@ -106,17 +97,5 @@ function clearMarkup() {
 
 function incrementPageNum() {
     pageNum += 1;
-    console.log(pageNum);
 };
-
-
-
-// async function addAndRenderBook() {
-//   try {
-//     const book = await addBook({});
-//     console.log(book);
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
 
